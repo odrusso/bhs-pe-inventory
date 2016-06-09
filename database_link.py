14d5b8e4f1cf330c2c022e6ee05aa5de89dc8347
Oscar <odrusso1@gmail.com>
2016-06-09 20:38:37 +1200

    Update
    Thingey

--- database_link.py
+++ database_link.py
@@ -1,4 +1,6 @@
+"""Module for connecting to the database, viewing, and inserting information."""
 import mysql.connector
+from sql_viewer import ErrorGUI, LoadingGUI
 
 class Database(object):
 
@@ -7,20 +9,28 @@ class Database(object):
         Data Attributes:    'db' is the database connection object
                             'cursor' is the database cursor object
         """
-
         try:
+            print("Connecting to Server...")
             self.db = mysql.connector.connect(**config)
-        except mysql.connector.Error as err:
+        except mysql.connector.Error as e:
             #Basic Error Management
-            print("There was an error connecting to the database.")
-            print(err)                                                          #Be sure to change this to an GUI error code eventually
+            error_popup = ErrorGUI(e)                                                        #Be sure to change this to an GUI error code eventually
         else:
             self.cursor = self.db.cursor()
+            print("Connected")
 
     def return_execution(self, query):
         """Returns the value of a SQL query"""
         self.cursor.execute(query)
         return self.cursor
+
+    def add_person(self, id, first_name, last_name):
+        query = "INSERT INTO `people` (`PersonID`, `FirstName`, `LastName`) VALUES ('{}', '{}', '{}')".format(id, first_name, last_name)
+        try:
+            self.cursor.execute(query)
+            self.db.commit()
+        except Exception as e:
+            error_popup = ErrorGUI(e)
 
 #Testing weather the database connections and can execute a basic query
 if __name__ == '__main__':
@@ -28,10 +38,11 @@ if __name__ == '__main__':
         #Database configuration and connection
         database_config = {
         "user": "root",
-        "password": "root",
-        "host": "127.0.0.1",
-        "database": "example"
+        "password": "5sQ-Hsd-ekt-bzS",
+        "host": "bhs-pe-inventory.ci3pushvdxiu.us-west-2.rds.amazonaws.com",
+        "database": "test"
         }
+        # 5sQ-Hsd-ekt-bzS
 
         ExampleDB = Database(database_config)                                   #Inniltalise the Database object
 
