ac5530c163d0b959c235ea700eaa52e2dfc7fe04
Oscar <odrusso1@gmail.com>
2016-08-24 13:31:17 +1200

    Deleted usergen.py
    usergen.py deleted as has been replaced by security.py

--- usergen.py
+++
@@ -1,9 +0,0 @@
-import scrypt
-import os
-
-password = input("Enter New Password: ")
-
-salt = str(os.urandom(16))
-print("Salt: " + salt)
-hash = str(scrypt.hash(password, salt))
-print("Hash: " + hash)
